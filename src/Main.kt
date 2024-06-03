c3962e052b3f74b7e593c5fb580824fdad886d25
Kabaji Egara <kabzegara@gmail.com>
2024-06-03 16:50:28 +0100

    feat: added ability to fetch mustache template

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,60 @@
+
+import kotlin.script.experimental.annotations.KotlinScript
+import kotlin.script.experimental.api.ResultValue
+import kotlin.script.experimental.api.valueOrThrow
+import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
+import kotlin.script.experimental.jvm.jvm
+import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
+import kotlin.script.experimental.jvmhost.createJvmCompilationConfigurationFromTemplate
+
+@KotlinScript
+abstract class LayoutScript
+
 fun main() {
-    val layout = layout {
-        button{
-            text = "Click me"
-            label = "Click me"
-            icon = "play"
-            type = "primary"
-            action{
-                type="play"
-            }
+    val builder = StringBuilder()
+    val compilationConfiguration = createJvmCompilationConfigurationFromTemplate<LayoutScript> {
+        jvm {
+            dependenciesFromCurrentContext(
+                wholeClasspath = true
+            )
         }
     }
-    val builder = StringBuilder()
-    layout.renderMustache(builder, "")
-    println("Final result")
-    println(builder.toString())
+
+    val layoutString = """
+         layout{
+            button{
+                text = "Click me"
+                label = "Click me"
+                icon = "play"
+                type = "primary"
+                action{
+                    type="play"
+                }
+            }
+        }
+    """.trimIndent()
+    val layoutScriptSource = layoutString.toScriptSource()
+
+    val result = BasicJvmScriptingHost().eval(layoutScriptSource,
+        compilationConfiguration,
+        null).valueOrThrow().returnValue;
+
+    println("Script evaluation result: ${result.scriptInstance}")
+
+    println("Starting script evaluation")
+    if (result !== null) {
+        val layout = (result as? ResultValue.Value)?.value as? Layout
+        if (layout != null) {
+            layout.renderMustache(builder, "")
+            println("Final result")
+            println(builder.toString())
+        } else {
+            println("Script did not return a Layout object")
+        }
+    } else {
+        println("Script evaluation failed: ")
+    }
 }
 
 fun layout(init: Layout.() -> Unit): Layout {
@@ -21,3 +62,4 @@ fun layout(init: Layout.() -> Unit): Layout {
     layout.init()
     return layout
 }
+
